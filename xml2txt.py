bbeffbf64e57b339212c1ed6b7edca81c8312a85
GGBOND <123057885+GG22bond@users.noreply.github.com>
2024-05-21 11:13:11 +0800

    Update xml2txt.py

--- xml2txt.py
+++ xml2txt.py
@@ -1,85 +1,129 @@
-import xml.etree.ElementTree as ET
-import os, cv2
-import numpy as np
+from xml.dom.minidom import Document
+import os
+import cv2
+from tqdm import tqdm
 import argparse
-from os import listdir
-from os.path import join
 
-classes = ['type1','type2','type3']
+def makexml(picPath, txtPath, xmlPath, datasetName):
 
-def convert(size, box):
-    dw = 1. / (size[0])
-    dh = 1. / (size[1])
-    x = (box[0] + box[1]) / 2.0 - 1
-    y = (box[2] + box[3]) / 2.0 - 1
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    x = x * dw
-    w = w * dw
-    y = y * dh
-    h = h * dh
-    return (x, y, w, h)
+    dic = {'0': "type1",
+           '1': "type2",
+           '2': "type3",
+           # ....
+           }
+    files = os.listdir(txtPath)
+    for i, name in enumerate(tqdm(files)):
+        if name == 'classes.txt':
+            continue
+        xmlBuilder = Document()
+        annotation = xmlBuilder.createElement("annotation")  # annotation标签
+        xmlBuilder.appendChild(annotation)
+        txtFile = open(os.path.join(txtPath, name))
+        txtList = txtFile.readlines()
+        img = cv2.imread(os.path.join(picPath, name.replace('txt', 'jpg')))
+        Pheight, Pwidth, Pdepth = img.shape
 
+        folder = xmlBuilder.createElement("folder")  # folder标签
+        foldercontent = xmlBuilder.createTextNode(datasetName)
+        folder.appendChild(foldercontent)
+        annotation.appendChild(folder)  # folder标签结束
 
-def convert_annotation(xmlpath, xmlname):
-    with open(xmlpath, "r", encoding='utf-8') as in_file:
-        txtname = xmlname[:-4] + '.txt'
-        txtfile = os.path.join(txtpath, txtname)
-        tree = ET.parse(in_file)
-        root = tree.getroot()
-        filename = root.find('filename')
-        img = cv2.imdecode(np.fromfile('{}/{}.{}'.format(imgpath, xmlname[:-4], postfix), np.uint8), cv2.IMREAD_COLOR)
-        h, w = img.shape[:2]
-        res = []
-        for obj in root.iter('object'):
-            cls = obj.find('name').text
-            if cls not in classes:
-                classes.append(cls)
-            cls_id = classes.index(cls)
-            xmlbox = obj.find('bndbox')
-            b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
-                 float(xmlbox.find('ymax').text))
-            bb = convert((w, h), b)
-            res.append(str(cls_id) + " " + " ".join([str(a) for a in bb]))
-        if len(res) != 0:
-            with open(txtfile, 'w+') as f:
-                f.write('\n'.join(res))
+        filename = xmlBuilder.createElement("filename")  # filename标签
+        filenamecontent = xmlBuilder.createTextNode(name[0:-4] + ".jpg")
+        filename.appendChild(filenamecontent)
+        annotation.appendChild(filename)  # filename标签结束
 
-'''
-python xml2txt.py --imgpath new/images/train --xmlpath new/annotations/train --txtpath new/labels/train
-python xml2txt.py --imgpath new/images/val --xmlpath new/annotations/val --txtpath new/labels/val
-'''
+        size = xmlBuilder.createElement("size")  # size标签
+        width = xmlBuilder.createElement("width")  # size子标签width
+        widthcontent = xmlBuilder.createTextNode(str(Pwidth))
+        width.appendChild(widthcontent)
+        size.appendChild(width)  # size子标签width结束
+
+        height = xmlBuilder.createElement("height")  # size子标签height
+        heightcontent = xmlBuilder.createTextNode(str(Pheight))
+        height.appendChild(heightcontent)
+        size.appendChild(height)  # size子标签height结束
+
+        depth = xmlBuilder.createElement("depth")  # size子标签depth
+        depthcontent = xmlBuilder.createTextNode(str(Pdepth))
+        depth.appendChild(depthcontent)
+        size.appendChild(depth)  # size子标签depth结束
+
+        annotation.appendChild(size)  # size标签结束
+
+        for j in txtList:
+            oneline = j.strip().split(" ")
+            object = xmlBuilder.createElement("object")  # object 标签
+            picname = xmlBuilder.createElement("name")  # name标签
+            namecontent = xmlBuilder.createTextNode(dic[oneline[0]])
+            picname.appendChild(namecontent)
+            object.appendChild(picname)  # name标签结束
+
+            pose = xmlBuilder.createElement("pose")  # pose标签
+            posecontent = xmlBuilder.createTextNode("Unspecified")
+            pose.appendChild(posecontent)
+            object.appendChild(pose)  # pose标签结束
+
+            truncated = xmlBuilder.createElement("truncated")  # truncated标签
+            truncatedContent = xmlBuilder.createTextNode("0")
+            truncated.appendChild(truncatedContent)
+            object.appendChild(truncated)  # truncated标签结束
+
+            difficult = xmlBuilder.createElement("difficult")  # difficult标签
+            difficultcontent = xmlBuilder.createTextNode("0")
+            difficult.appendChild(difficultcontent)
+            object.appendChild(difficult)  # difficult标签结束
+
+            bndbox = xmlBuilder.createElement("bndbox")  # bndbox标签
+            xmin = xmlBuilder.createElement("xmin")  # xmin标签
+            mathData = int(((float(oneline[1])) * Pwidth + 1) - (float(oneline[3])) * 0.5 * Pwidth)
+            xminContent = xmlBuilder.createTextNode(str(mathData))
+            xmin.appendChild(xminContent)
+            bndbox.appendChild(xmin)  # xmin标签结束
+
+            ymin = xmlBuilder.createElement("ymin")  # ymin标签
+            mathData = int(((float(oneline[2])) * Pheight + 1) - (float(oneline[4])) * 0.5 * Pheight)
+            yminContent = xmlBuilder.createTextNode(str(mathData))
+            ymin.appendChild(yminContent)
+            bndbox.appendChild(ymin)  # ymin标签结束
+
+            xmax = xmlBuilder.createElement("xmax")  # xmax标签
+            mathData = int(((float(oneline[1])) * Pwidth + 1) + (float(oneline[3])) * 0.5 * Pwidth)
+            xmaxContent = xmlBuilder.createTextNode(str(mathData))
+            xmax.appendChild(xmaxContent)
+            bndbox.appendChild(xmax)  # xmax标签结束
+
+            ymax = xmlBuilder.createElement("ymax")  # ymax标签
+            mathData = int(((float(oneline[2])) * Pheight + 1) + (float(oneline[4])) * 0.5 * Pheight)
+            ymaxContent = xmlBuilder.createTextNode(str(mathData))
+            ymax.appendChild(ymaxContent)
+            bndbox.appendChild(ymax)  # ymax标签结束
+
+            object.appendChild(bndbox)  # bndbox标签结束
+
+            annotation.appendChild(object)  # object标签结束
+
+        f = open(os.path.join(xmlPath, name.replace('txt', 'xml')), 'w')
+        xmlBuilder.writexml(f, indent='\t', newl='\n', addindent='\t', encoding='utf-8')
+        f.close()
+
 
 if __name__ == "__main__":
 
+    # python txt2xml.py --imgpath ./valid/images --txtpath ./valid/labels --xmlpath ./valid/ann
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--postfix', default='jpg')
-    parser.add_argument('--imgpath', type=str, default='data_amp_FIN/images/val')
-    parser.add_argument('--xmlpath', type=str, default='data_amp_FIN/Annotations/val')
-    parser.add_argument('--txtpath', type=str, default='data_amp_FIN/labels/val')
+
+    parser.add_argument('--imgpath', type= str, default='./valid/images')  # 原图文件夹路径
+    parser.add_argument('--txtpath', type=str, default='./valid/labels')   # 原txt标签文件夹路径
+    parser.add_argument('--xmlpath', type=str, default='./valid/ann')   # 保存xml文件夹路径
+    parser.add_argument('--datasetname',type=str, default='dataset')  # 数据集名称
     args = parser.parse_args()
 
-    postfix = args.postfix
     imgpath = args.imgpath
-    xmlpath = args.xmlpath
     txtpath = args.txtpath
+    xmlpath = args.xmlpath
+    datasetName = args.datasetname
 
-    if not os.path.exists(txtpath):
-        os.makedirs(txtpath, exist_ok=True)
-    
-    list = os.listdir(xmlpath)
-    error_file_list = []
-    for i in range(0, len(list)):
-        try:
-            path = os.path.join(xmlpath, list[i])
-            if ('.xml' in path) or ('.XML' in path):
-                convert_annotation(path, list[i])
-                print(f'file {list[i]} convert success.')
-            else:
-                print(f'file {list[i]} is not xml format.')
-        except Exception as e:
-            print(f'file {list[i]} convert error.')
-            print(f'error message:\n{e}')
-            error_file_list.append(list[i])
-    print(f'this file convert failure\n{error_file_list}')
-    print(f'Dataset Classes:{classes}')
+    os.makedirs(xmlpath) if not os.path.exists(xmlpath) else None
+    makexml(imgpath, txtpath, xmlpath, datasetName)
