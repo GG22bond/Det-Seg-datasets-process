56c06f5fd3a85c2ce2d0f1ddfd20db10e49753c0
GGBOND <123057885+GG22bond@users.noreply.github.com>
2025-06-09 10:43:23 +0800

    txt2xml.py

--- txt2xml.py
+++ txt2xml.py
@@ -4,124 +4,102 @@ import cv2
 from tqdm import tqdm
 import argparse
 
-def makexml(picPath, txtPath, xmlPath, datasetName):
+classes = ['type1', 'type2', 'type3']
 
-    dic = {'0': "type1",
-           '1': "type2",
-           '2': "type3",
-           # ....
-           }
+def makexml(picPath, txtPath, xmlPath):
+    datasetName = 'dataset'
     files = os.listdir(txtPath)
     for i, name in enumerate(tqdm(files)):
         if name == 'classes.txt':
             continue
+
         xmlBuilder = Document()
-        annotation = xmlBuilder.createElement("annotation")  # annotation标签
+        annotation = xmlBuilder.createElement("annotation")
         xmlBuilder.appendChild(annotation)
+
         txtFile = open(os.path.join(txtPath, name))
         txtList = txtFile.readlines()
-        img = cv2.imread(os.path.join(picPath, name.replace('txt', 'jpg')))
+        txtFile.close()
+
+        img_path = os.path.join(picPath, name.replace('.txt', '.jpg'))
+        img = cv2.imread(img_path)
+        if img is None:
+            print(f"Warning: Unable to read image {img_path}, skipping.")
+            continue
         Pheight, Pwidth, Pdepth = img.shape
 
-        folder = xmlBuilder.createElement("folder")  # folder标签
-        foldercontent = xmlBuilder.createTextNode(datasetName)
-        folder.appendChild(foldercontent)
-        annotation.appendChild(folder)  # folder标签结束
+        # folder
+        folder = xmlBuilder.createElement("folder")
+        folder.appendChild(xmlBuilder.createTextNode(datasetName))
+        annotation.appendChild(folder)
 
-        filename = xmlBuilder.createElement("filename")  # filename标签
-        filenamecontent = xmlBuilder.createTextNode(name[0:-4] + ".jpg")
-        filename.appendChild(filenamecontent)
-        annotation.appendChild(filename)  # filename标签结束
+        # filename
+        filename = xmlBuilder.createElement("filename")
+        filename.appendChild(xmlBuilder.createTextNode(name[:-4] + ".jpg"))
+        annotation.appendChild(filename)
 
-        size = xmlBuilder.createElement("size")  # size标签
-        width = xmlBuilder.createElement("width")  # size子标签width
-        widthcontent = xmlBuilder.createTextNode(str(Pwidth))
-        width.appendChild(widthcontent)
-        size.appendChild(width)  # size子标签width结束
+        # size
+        size = xmlBuilder.createElement("size")
+        for tag, val in [("width", Pwidth), ("height", Pheight), ("depth", Pdepth)]:
+            elem = xmlBuilder.createElement(tag)
+            elem.appendChild(xmlBuilder.createTextNode(str(val)))
+            size.appendChild(elem)
+        annotation.appendChild(size)
 
-        height = xmlBuilder.createElement("height")  # size子标签height
-        heightcontent = xmlBuilder.createTextNode(str(Pheight))
-        height.appendChild(heightcontent)
-        size.appendChild(height)  # size子标签height结束
+        # objects
+        for line in txtList:
+            parts = line.strip().split()
+            if len(parts) != 5:
+                continue
+            cls_id, x_center, y_center, w_norm, h_norm = parts
+            cls_idx = int(cls_id)
+            if cls_idx < 0 or cls_idx >= len(classes):
+                print(f"Warning: class index {cls_idx} out of range, skipping.")
+                continue
 
-        depth = xmlBuilder.createElement("depth")  # size子标签depth
-        depthcontent = xmlBuilder.createTextNode(str(Pdepth))
-        depth.appendChild(depthcontent)
-        size.appendChild(depth)  # size子标签depth结束
+            obj = xmlBuilder.createElement("object")
+            # name
+            name_tag = xmlBuilder.createElement("name")
+            name_tag.appendChild(xmlBuilder.createTextNode(classes[cls_idx]))
+            obj.appendChild(name_tag)
+            # pose, truncated, difficult
+            for tag, text in [("pose", "Unspecified"), ("truncated", "0"), ("difficult", "0")]:
+                elem = xmlBuilder.createElement(tag)
+                elem.appendChild(xmlBuilder.createTextNode(text))
+                obj.appendChild(elem)
 
-        annotation.appendChild(size)  # size标签结束
+            # bndbox
+            bndbox = xmlBuilder.createElement("bndbox")
+            x_center_f = float(x_center) * Pwidth + 1
+            y_center_f = float(y_center) * Pheight + 1
+            half_w = float(w_norm) * 0.5 * Pwidth
+            half_h = float(h_norm) * 0.5 * Pheight
+            coords = {
+                "xmin": int(max(0, x_center_f - half_w)),
+                "ymin": int(max(0, y_center_f - half_h)),
+                "xmax": int(min(Pwidth, x_center_f + half_w)),
+                "ymax": int(min(Pheight, y_center_f + half_h)),
+            }
+            for tag, val in coords.items():
+                elem = xmlBuilder.createElement(tag)
+                elem.appendChild(xmlBuilder.createTextNode(str(val)))
+                bndbox.appendChild(elem)
+            obj.appendChild(bndbox)
+            annotation.appendChild(obj)
 
-        for j in txtList:
-            oneline = j.strip().split(" ")
-            object = xmlBuilder.createElement("object")  # object 标签
-            picname = xmlBuilder.createElement("name")  # name标签
-            namecontent = xmlBuilder.createTextNode(dic[oneline[0]])
-            picname.appendChild(namecontent)
-            object.appendChild(picname)  # name标签结束
-
-            pose = xmlBuilder.createElement("pose")  # pose标签
-            posecontent = xmlBuilder.createTextNode("Unspecified")
-            pose.appendChild(posecontent)
-            object.appendChild(pose)  # pose标签结束
-
-            truncated = xmlBuilder.createElement("truncated")  # truncated标签
-            truncatedContent = xmlBuilder.createTextNode("0")
-            truncated.appendChild(truncatedContent)
-            object.appendChild(truncated)  # truncated标签结束
-
-            difficult = xmlBuilder.createElement("difficult")  # difficult标签
-            difficultcontent = xmlBuilder.createTextNode("0")
-            difficult.appendChild(difficultcontent)
-            object.appendChild(difficult)  # difficult标签结束
-
-            bndbox = xmlBuilder.createElement("bndbox")  # bndbox标签
-            xmin = xmlBuilder.createElement("xmin")  # xmin标签
-            mathData = int(((float(oneline[1])) * Pwidth + 1) - (float(oneline[3])) * 0.5 * Pwidth)
-            xminContent = xmlBuilder.createTextNode(str(mathData))
-            xmin.appendChild(xminContent)
-            bndbox.appendChild(xmin)  # xmin标签结束
-
-            ymin = xmlBuilder.createElement("ymin")  # ymin标签
-            mathData = int(((float(oneline[2])) * Pheight + 1) - (float(oneline[4])) * 0.5 * Pheight)
-            yminContent = xmlBuilder.createTextNode(str(mathData))
-            ymin.appendChild(yminContent)
-            bndbox.appendChild(ymin)  # ymin标签结束
-
-            xmax = xmlBuilder.createElement("xmax")  # xmax标签
-            mathData = int(((float(oneline[1])) * Pwidth + 1) + (float(oneline[3])) * 0.5 * Pwidth)
-            xmaxContent = xmlBuilder.createTextNode(str(mathData))
-            xmax.appendChild(xmaxContent)
-            bndbox.appendChild(xmax)  # xmax标签结束
-
-            ymax = xmlBuilder.createElement("ymax")  # ymax标签
-            mathData = int(((float(oneline[2])) * Pheight + 1) + (float(oneline[4])) * 0.5 * Pheight)
-            ymaxContent = xmlBuilder.createTextNode(str(mathData))
-            ymax.appendChild(ymaxContent)
-            bndbox.appendChild(ymax)  # ymax标签结束
-
-            object.appendChild(bndbox)  # bndbox标签结束
-
-            annotation.appendChild(object)  # object标签结束
-
-        f = open(os.path.join(xmlPath, name.replace('txt', 'xml')), 'w')
-        xmlBuilder.writexml(f, indent='\t', newl='\n', addindent='\t', encoding='utf-8')
-        f.close()
-
+        # write XML
+        xml_file = os.path.join(xmlPath, name.replace('.txt', '.xml'))
+        with open(xml_file, 'w', encoding='utf-8') as f:
+            xmlBuilder.writexml(f, indent='\t', newl='\n', addindent='\t')
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--imgpath', type= str, default='./images')  # 原图文件夹路径
-    parser.add_argument('--txtpath', type=str, default='./labels')   # 原txt标签文件夹路径
-    parser.add_argument('--xmlpath', type=str, default='./Annotations')   # 保存xml文件夹路径
-    parser.add_argument('--datasetname',type=str, default='dataset')  # 数据集名称
+    parser = argparse.ArgumentParser(description="Convert YOLO txt annotations to Pascal VOC XML.")
+    parser.add_argument('--imgpath', type=str, default='./test/images', help='Path to images')
+    parser.add_argument('--txtpath', type=str, default='./test/labels', help='Path to YOLO txt labels')
+    parser.add_argument('--xmlpath', type=str, default='./test/3', help='Output path for XML')
     args = parser.parse_args()
 
-    imgpath = args.imgpath
-    txtpath = args.txtpath
-    xmlpath = args.xmlpath
-    datasetName = args.datasetname
+    os.makedirs(args.xmlpath, exist_ok=True)
 
-    os.makedirs(xmlpath) if not os.path.exists(xmlpath) else None
-    makexml(imgpath, txtpath, xmlpath, datasetName)
+    makexml(args.imgpath, args.txtpath, args.xmlpath)
